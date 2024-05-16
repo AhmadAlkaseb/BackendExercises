c5fc38b6828acf81dfbe6b6ac06d3efb0193905d
AhmadAlkaseb <ahmadalkasebpri@icloud.com>
2024-05-16 17:08:14 +0200

    .

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -2,15 +2,15 @@ package rest;
 
 import Controllers.ISecurityController;
 import Controllers.SecurityController;
-import dtos.UserDTO;
-import exceptions.ApiException;
-import persistence.HibernateConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import dtos.UserDTO;
+import exceptions.ApiException;
 import io.javalin.Javalin;
 import io.javalin.apibuilder.EndpointGroup;
 import io.javalin.http.HttpStatus;
 import jakarta.persistence.EntityManagerFactory;
+import persistence.HibernateConfig;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -23,21 +23,17 @@ public class ApplicationConfig {
     private Javalin app;
 
     private ApplicationConfig() {
-        // Initialize ObjectMapper
         ObjectMapper om = new ObjectMapper();
-
-        // Initialize Javalin app
         app = Javalin.create(config -> {
             config.http.defaultContentType = "application/json";
             config.routing.contextPath = "/api";
             config.plugins.enableCors(cors -> {
                 cors.add(it -> {
-                    it.anyHost();
+                    it.allowedOrigins().add("https://cphbusinessprojekt.dk");
                 });
             });
         });
     }
-
 
     public static ApplicationConfig getInstance() {
         if (instance == null) {
@@ -61,7 +57,6 @@ public class ApplicationConfig {
     }
 
     public ApplicationConfig setExceptionHandlers() {
-
         app.exception(Exception.class, (e, ctx) -> {
             ObjectNode node = om.createObjectNode().put("errorMessage", e.getMessage());
             ctx.status(500).json(node);
@@ -74,20 +69,14 @@ public class ApplicationConfig {
         app.exception(IllegalStateException.class, (e, ctx) -> {
             ctx.status(400).result("Bad Request: " + e.getMessage());
         });
-
         return instance;
     }
 
     public ApplicationConfig checkSecurityRoles() {
-        // Check roles on the user (ctx.attribute("username") and compare with permittedRoles using securityController.authorize()
         app.updateConfig(config -> {
-
             config.accessManager((handler, ctx, permittedRoles) -> {
-                // permitted roles are defined in the last arg to routes: get("/", ctx -> ctx.result("Hello World"), Role.ANYONE);
-
                 Set<String> allowedRoles = permittedRoles.stream().map(role -> role.toString().toUpperCase()).collect(Collectors.toSet());
                 if (allowedRoles.contains("ANYONE") || ctx.method().toString().equals("OPTIONS")) {
-                    // Allow requests from anyone and OPTIONS requests (preflight in CORS)
                     handler.handle(ctx);
                     return;
                 }
